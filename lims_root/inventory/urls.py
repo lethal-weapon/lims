309262b11bd8c2c2b4da1891ac8f69bdda5aa21c
Jacob Wong <jacobwong26@gmail.com>
2020-04-30 06:45:16 +0800

    Create 'add facility to application list' window

--- lims_root/inventory/urls.py
+++ lims_root/inventory/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('add-facility-to-list/', views.add_facility_to_list,
+         name='add-facility-to-list'),
+
     path('', views.warehouse, name='warehouse'),
 ]
--- lims_root/inventory/views.py
+++ lims_root/inventory/views.py
@@ -2,13 +2,35 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from .models import Apparatus, Laboratory
+from applications.models import FacilityApplication
+from .models import Apparatus, Facility, Laboratory
 
 
+# Only authenticated and verified users can view the inventory
 @login_required(login_url=reverse_lazy('login'))
 def warehouse(request):
-    # only authenticated and verified users can view the inventory
+    # Find out what facilities are unavailable
+    fapps = FacilityApplication.objects.filter(status__in=['WAI', 'BOR', 'OVE'])
+    in_use_facility_ids = []
+
+    for fapp in fapps:
+        for f in fapp.items.all():
+            in_use_facility_ids.append(f.id)
+
     return render(request, 'inventory/warehouse.html', {
-        'apparatus_list' : Apparatus.objects.all(),
-        'laboratory_list': Laboratory.objects.all(),
+        'facility_list'            : Facility.objects.exclude(id__in=in_use_facility_ids),
+        'apparatus_list'           : Apparatus.objects.exclude(id__in=in_use_facility_ids),
+        'laboratory_list'          : Laboratory.objects.exclude(id__in=in_use_facility_ids),
+        'facility_application_list': FacilityApplication.objects.filter(
+            applicant=request.user
+        ).filter(
+            status='PEN'
+        ),
     })
+
+
+# Add a facility to 'items' field of an
+# application list whose status is PENDING
+@login_required(login_url=reverse_lazy('login'))
+def add_facility_to_list(request):
+    pass
